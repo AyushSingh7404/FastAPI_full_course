524de1917d05b6fc535a27e9565a99a978a33b33
AyushSingh7404 <rajputayushsingh07042004@gmail.com>
2026-02-11 14:55:14 +0530

    Completed the basics of CRUD operations and how to use pydantic properly and what are various HTTP response signals.

--- README.md
+++ README.md
@@ -9,3 +9,23 @@
 # By default the server runs on this port -> "http://127.0.0.1:8000"
 
 # One thing to remember is that the order of APIs do matter if there are multiple apis of same routes and methods.
+
+# Here is an example of how order of the routes matter because if we have a route which is /posts/{id} and another route which is /posts/latest then if we put the /posts/{id} route before the /posts/latest route then when we try to access the /posts/latest route it will give us an error because it will try to match the /posts/latest route with the /posts/{id} route and it will not find any match and it will give us an error. So we have to put the /posts/latest route before the /posts/{id} route so that it can match the /posts/latest route first and then it will not give us an error.
+
+# We need a way to validate whether we get correct data in request and send correct in response, this is why we use "Pydantic".
+# Pydantic is a python that is used to validate data (thanks to it's BaseModel).
+
+# Pydantic also automatically coverts data intp proper datatype if possible.
+# Like if we send "7" in a variable where only int in allowed then here pydantic will perform automatic type conversion.
+
+# The data that we get from the body in the response is in the form of pydantic model and we can convert it into dict using the dict() method.
+
+# There are 4 types of oeperations(CRUD):
+1. Create (Post) --> Add new data in the database.
+2. Read (Get) --> Get data from database (either get all or get by id).
+3. Update (Put) --> Update data in database.
+4. Delete (Delete) --> Delete data from database.
+
+# If we ever pass an array in response of a request, then pydantic will automatically converts it in json so that it is properly structured
+
+# By default FastAPI provides documentation on "/docs" and on "/redoc".
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -9,6 +10,13 @@ class Post(BaseModel):
     content:str
     published: bool = True
     rating: Optional[int] = None
+
+
+my_posts = [
+    {"title": "First post", "content": "This is the content of the first post", "published": True, "rating": 5, "id": 1},
+    {"title": "Second post", "content": "This is the content of the second post", "published": False, "rating": 4, "id": 2},
+    {"title": "Third post", "content": "This is the content of the third post", "published": True, "rating": 3, "id": 3}
+]
 
 
 # # Here the order of the the apis matter because we have 2 routes which are same.
@@ -22,14 +30,55 @@ async def root():
     return {"message": "Hii Guys Welcome back"}
 
 
-@app.get("/post")
+@app.get("/posts")
 async def get_post():
-    return {"post": "My post"}
+    return {"post": my_posts}   # If we ever pass an array in response of a request, then pydantic will automatically converts it in json so that it is properly structured
 
 
-@app.post("/create_posts")
-async def create_posts(data: Post):
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+async def create_posts(post: Post):
     # new = data.dict()    # The data that we are getting from the body is in the form of pydantic model and we can convert it into dict using the dict() method.
     # print(new)
-    return f"Created post with title: {data.title}, and content: {data.content}, and published: {data.published}, and rating: {data.rating}"    
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    print(my_posts)
+    return {"data": post_dict}
 
+
+@app.get("/posts/{id}")
+async def get_post(id: int, response: Response):  # Here if we do not set id to int we will get an error because we are comparing the id with the id in the list which is of type int and if we do not set it to int then it will be of type str and we will get an error because we cannot compare str with int.
+    for post in my_posts:
+        if post['id'] == id:
+            return {"post_detail": post}
+    
+    # if post['id'] != id:    # Here this is a broken logic as the variable post is never defined outside the above for loop but in python the check variable exists outside the for loop with the last checked value. So this is how this works.
+    #     response.status_code = status.HTTP_404_NOT_FOUND
+    #     return {"message": f"Post with id {id} not found!"}
+
+    # response.status_code = status.HTTP_404_NOT_FOUND
+    # return {"message": f"Post with id {id} not found!"}
+    
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found!")   # This is the better way to handle the error as it is more readable and we can also pass the detail of the error in the response.
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id: int):
+    for i, post in enumerate(my_posts):
+        if post['id'] == id:
+            my_posts.pop(i)
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+    
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} does not exist!")   # Here we are returning the response with the status code and the content of the response which is the error message.
+
+
+@app.put("/posts/{id}")
+async def update_post(id: int, post: Post):
+    post_dict = post.dict()
+    post_dict["id"] = id
+    for i, post in enumerate(my_posts):
+        if post['id'] == id:
+            my_posts[i] = post_dict
+            return {"message": f"Post with id {id} updated successfully!", "post": post_dict}
+    
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} does not exist!")   # Here we are returning the response with the status code and the content of the response which is the error message.
